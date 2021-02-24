f7d14c7c39da080fc70780d507e11444d0f3543e
-GENEVE- <geneve@csu.edu.cn>
2021-02-24 22:10:55 +0800

    feat:完成每日一题832  feat:完成题目11、12

---
+++ src/com/geneve/leetcode/editor/cn/IntegerToRoman.java
@@ -0,0 +1,154 @@
+// 12-整数转罗马数字
+//罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。 
+//
+// 
+//字符          数值
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000 
+//
+// 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做 XXVII, 即为 XX + V + I
+//I 。 
+//
+// 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5
+// 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况： 
+//
+// 
+// I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。 
+// X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+// C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。 
+// 
+//
+// 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。 
+//
+// 示例 1: 
+//
+// 
+//输入: 3
+//输出: "III" 
+//
+// 示例 2: 
+//
+// 
+//输入: 4
+//输出: "IV" 
+//
+// 示例 3: 
+//
+// 
+//输入: 9
+//输出: "IX" 
+//
+// 示例 4: 
+//
+// 
+//输入: 58
+//输出: "LVIII"
+//解释: L = 50, V = 5, III = 3.
+// 
+//
+// 示例 5: 
+//
+// 
+//输入: 1994
+//输出: "MCMXCIV"
+//解释: M = 1000, CM = 900, XC = 90, IV = 4. 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= num <= 3999 
+// 
+// Related Topics 数学 字符串 
+// 👍 494 👎 0
+
+package com.geneve.leetcode.editor.cn;
+
+public class IntegerToRoman {
+
+  public static void main(String[] args) {
+    Solution solution = new IntegerToRoman().new Solution();
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+
+    public String intToRoman(int num) {
+      int thu = num / 1000;
+      int hun = (num % 1000) / 100;
+      int ten = (num % 100) / 10;
+      int dec = num % 10;
+      StringBuilder builder = new StringBuilder();
+
+      for (int i = 0; i < thu; i++) {
+        builder.append("M");
+      }
+
+      switch (hun) {
+        case 9:
+          builder.append("CM");
+          break;
+        case 4:
+          builder.append("CD");
+          break;
+        default:
+          int h = hun;
+          if (h >= 5) {
+            builder.append("D");
+            h -= 5;
+          }
+          for (int i = 0; i < h; i++) {
+            builder.append("C");
+          }
+          break;
+      }
+
+      switch (ten){
+        case 9:
+          builder.append("XC");
+          break;
+        case 4:
+          builder.append("XL");
+          break;
+        default:
+          int t = ten;
+          if (t >= 5) {
+            builder.append("L");
+            t -= 5;
+          }
+          for (int i = 0; i < t; i++) {
+            builder.append("X");
+          }
+          break;
+      }
+
+      switch (dec){
+        case 9:
+          builder.append("IX");
+          break;
+        case 4:
+          builder.append("IV");
+          break;
+        default:
+          int d = dec;
+          if (d >= 5) {
+            builder.append("V");
+            d -= 5;
+          }
+          for (int i = 0; i < d; i++) {
+            builder.append("I");
+          }
+          break;
+      }
+      return builder.toString();
+    }
+
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+}
